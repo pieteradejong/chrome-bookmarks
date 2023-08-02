70bf642df887be183bae504cba7574d4afbbfae5
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-02 15:42:14 -0400

    wrap json load into try catch

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import json
 
 CHROME_PROFILE_NAME: str = "Profile 1"
-CHROME_BOOKMARKS_FILE: str = f"/Users/pieterdejong/Library/Application Support/Google/Chrome/{CHROME_PROFILE_NAME}/Bookmarks"
+CHROME_BOOKMARKS_FILE_PATH: str = f"/Users/pieterdejong/Library/Application Support/Google/Chrome/{CHROME_PROFILE_NAME}/Bookmarks"
 
-def read_file_to_json(CHROME_BOOKMARKS_FILE):
-    with open(CHROME_BOOKMARKS_FILE) as f:
-        data = json.load(f)
-        return data
+def load_json_from_file(filepath: str) -> dict:
+    with open(filepath, 'r') as f:
+        try:
+             bookmarks: dict = json.load(f)
+             return bookmarks
+        except json.JSONDecodeError as e:
+             raise ValueError(f"Error: Failed to convert JSON to dictionary. Reason: {e}")
 
 def preview(data, n: int=1000) -> None:
     print(f"Previewing first {n} characters of data...\n")
     print(json.dumps(data, indent=4)[:n])
 
 def main():
-    print("Starting bookmark analysis...\n")
-    data = read_file_to_json(CHROME_BOOKMARKS_FILE)
+    print(f"Starting bookmark analysis...\n")
+    data: dict = load_json_from_file(CHROME_BOOKMARKS_FILE_PATH)
     preview(data, n=1000)
-    print("Analysis complete...\n")
+    print(f"Analysis complete...\n")
 
 if __name__ == "__main__":
     main()
