85f1fe1bcac726c703ed21d28a159b76c4a08ced
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-02 16:14:10 -0400

    applied formatting rules from black package

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import json
 CHROME_PROFILE_NAME: str = "Profile 1"
 CHROME_BOOKMARKS_FILE_PATH: str = f"/Users/pieterdejong/Library/Application Support/Google/Chrome/{CHROME_PROFILE_NAME}/Bookmarks"
 
-def load_json_from_file(filepath: str) -> dict:
-    with open(filepath, 'r') as f:
-        try:
-             bookmarks: dict = json.load(f)
-             return bookmarks
-        except json.JSONDecodeError as e:
-             raise ValueError(f"Error: Failed to convert JSON to dictionary. Reason: {e}")
 
-def preview(data, n: int=1000) -> None:
+def load_json_from_file(filepath: str) -> dict:
+    with open(filepath, "r") as f:
+        try:
+            bookmarks: dict = json.load(f)
+            return bookmarks
+        except json.JSONDecodeError as e:
+            raise ValueError(
+                f"Error: Failed to convert JSON to dictionary. Reason: {e}"
+            )
+
+
+def preview(data, n: int = 1000) -> None:
     print(f"Previewing first {n} characters of data...\n")
     print(json.dumps(data, indent=4)[:n])
+
 
 def main():
     print(f"Starting bookmark analysis...\n")
     data: dict = load_json_from_file(CHROME_BOOKMARKS_FILE_PATH)
+
     preview(data, n=1000)
     print(f"Analysis complete...\n")
+
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,5 +1,22 @@
+import json
+import main
 import pytest
-# TODO tests to write:
-# 1. test correct loading of json into dictionary
-# 3. test that invalid json raises ValueError
-# 2. test that previewing of json prints correct output
+
+# test that previewing of json prints correct output
+
+
+def test_load_valid_json_into_dict():
+    try:
+        data_dict = main.load_json_from_file(main.CHROME_BOOKMARKS_FILE_PATH)
+    except ValueError as e:
+        pytest.fail(f"Failed to load JSON from file: {e}")
+
+    assert isinstance(data_dict, dict), "JSON data should be loaded as a dictionary"
+
+
+def test_loading_invalid_json():
+    filepath: str = f"json_invalid"
+    with pytest.raises(
+        ValueError, match=r"Failed to convert JSON to dictionary\. Reason: .*"
+    ):
+        main.load_json_from_file(filepath)
