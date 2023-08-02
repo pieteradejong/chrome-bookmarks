8b75830d90ac5745635f0939331b31554ff46cf3
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-02 15:42:33 -0400

    add test stubs

---
+++ test_main.py
@@ -0,0 +1,5 @@
+import pytest
+# TODO tests to write:
+# 1. test correct loading of json into dictionary
+# 3. test that invalid json raises ValueError
+# 2. test that previewing of json prints correct output
